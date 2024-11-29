22447428b8c9c79443198f28cce355460c4c1d40
maks <nazwiskomaks29@gmail.com>
2024-11-29 23:09:00 +0100

    nav-menu 1.3 update (almost finished)

--- nav-menu.js
+++ nav-menu.js
@@ -1,5 +1,9 @@
 const toast = document.querySelector(".toast");
 
+const menu = document.querySelector(".menu");
+const filter = document.querySelector(".filter");
+const container = document.querySelector(".dropdown-menu-container");
+
 const copyPhone = () => {
   navigator.clipboard.writeText("+44 7949 940893");
   toast.classList.add("copied");
@@ -7,3 +11,19 @@ const copyPhone = () => {
     toast.classList.remove("copied");
   }, 2000);
 };
+
+function closeMenu() {
+  menu.classList.remove("opened");
+  filter.classList.remove("opened");
+  setTimeout(() => {
+    container.classList.remove("opened");
+  }, 750);
+}
+
+function openMenu() {
+  container.classList.add("opened");
+  setTimeout(() => {
+    menu.classList.add("opened");
+    filter.classList.add("opened");
+  }, 10);
+}
