32d218319bfd28513d34f2cf7a2184d169b5d2ac
maks <nazwiskomaks29@gmail.com>
2024-12-22 21:23:23 +0100

    contact page 1.0

---
+++ public/js/contact.js
@@ -0,0 +1,28 @@
+const input1 = document.querySelector(".inp1");
+const label1 = document.querySelector(".l1");
+const input2 = document.querySelector(".inp2");
+const label2 = document.querySelector(".l2");
+
+const toggleClass = () => {
+  if (input1.value.trim() || document.activeElement === input1) {
+    label1.classList.add("selected");
+  } else {
+    label1.classList.remove("selected");
+  }
+};
+
+const toggleClass2 = () => {
+  if (input2.value.trim() || document.activeElement === input2) {
+    label2.classList.add("selected");
+  } else {
+    label2.classList.remove("selected");
+  }
+};
+
+input1.addEventListener("focus", toggleClass);
+input1.addEventListener("input", toggleClass);
+input1.addEventListener("blur", toggleClass);
+
+input2.addEventListener("focus", toggleClass2);
+input2.addEventListener("input", toggleClass2);
+input2.addEventListener("blur", toggleClass2);
