954aa422d69802f1390e0697c237f237bf4607f4
maks <nazwiskomaks29@gmail.com>
2025-01-19 19:40:53 +0100

    form update

--- public/js/rent-to-rent.js
+++ public/js/rent-to-rent.js
@@ -1,29 +1,33 @@
-const input1 = document.querySelector(".inp1");
-const label1 = document.querySelector(".l1");
-const input2 = document.querySelector(".inp2");
-const label2 = document.querySelector(".l2");
-const messageTitle = document.getElementById("title");
+const input1 = document.querySelector("#email");
+const label1 = document.querySelector(".l-email");
+const input2 = document.querySelector(".phone");
+const label2 = document.querySelector(".l-phone");
 
-const toggleClass = () => {
-  if (input1.value.trim() || document.activeElement === input1) {
-    label1.classList.add("selected");
+const input3 = document.querySelector(".first-name");
+const label3 = document.querySelector(".l-fn");
+const input4 = document.querySelector(".last-name");
+const label4 = document.querySelector(".l-ln");
+
+input1.addEventListener("focus", () => toggleClass(input1, label1));
+input1.addEventListener("input", () => toggleClass(input1, label1));
+input1.addEventListener("blur", () => toggleClass(input1, label1));
+
+input2.addEventListener("focus", () => toggleClass(input2, label2));
+input2.addEventListener("input", () => toggleClass(input2, label2));
+input2.addEventListener("blur", () => toggleClass(input2, label2));
+
+input3.addEventListener("focus", () => toggleClass(input3, label3));
+input3.addEventListener("input", () => toggleClass(input3, label3));
+input3.addEventListener("blur", () => toggleClass(input3, label3));
+
+input4.addEventListener("focus", () => toggleClass(input4, label4));
+input4.addEventListener("input", () => toggleClass(input4, label4));
+input4.addEventListener("blur", () => toggleClass(input4, label4));
+
+const toggleClass = (input, label) => {
+  if (input.value.trim() || document.activeElement === input) {
+    label.classList.add("selected");
   } else {
-    label1.classList.remove("selected");
+    label.classList.remove("selected");
   }
 };
-
-const toggleClass2 = () => {
-  if (input2.value.trim() || document.activeElement === input2) {
-    label2.classList.add("selected");
-  } else {
-    label2.classList.remove("selected");
-  }
-};
-
-input1.addEventListener("focus", toggleClass);
-input1.addEventListener("input", toggleClass);
-input1.addEventListener("blur", toggleClass);
-
-input2.addEventListener("focus", toggleClass2);
-input2.addEventListener("input", toggleClass2);
-input2.addEventListener("blur", toggleClass2);
