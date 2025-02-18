c22d623105f2c2e3221b685a600a1c5b635d38de
maks <nazwiskomaks29@gmail.com>
2025-02-18 18:58:24 +0100

    prettier

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pagesRouter = require('./pages');
-const apiRouter = require('./api');
+const pagesRouter = require("./pages");
+const apiRouter = require("./api");
 
-router.use('/', pagesRouter);
-router.use('/api', apiRouter);
+router.use("/", pagesRouter);
+router.use("/api", apiRouter);
 
 module.exports = router;
